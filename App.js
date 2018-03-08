5959a4a78c1cabf72fafa70fa3917577011e8d65
Gregory Lee <gregliest@gmail.com>
2018-03-08 09:31:57 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const awApi = require('./model/api/awApi')
+
+export default class App extends React.Component {
+  render() {
+      
+      awApi.getReachesBySearch('')
+      .then(function(results){
+          console.log(results)
+      })
+          
+    return (
+      <View style={styles.container}>
+        <Text>This is American Whitewater</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
