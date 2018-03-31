9554e1aa89dd84ffe6b8c2e56ca6ecf9eb097fd9
Alex Kerney <abk@mac.com>
2018-03-30 20:21:08 -0400

    Shortening some line lengths

--- aw/Helpers/AWApiHelper.swift
+++ aw/Helpers/AWApiHelper.swift
@@ -54,13 +54,21 @@ struct AWApiHelper {
     static func conditionFromApi(condition: String) -> Condition {
         switch condition {
         case "low":
-            return Condition(name: "Low", color: UIColor(named: "status_yellow")!, icon: UIImage(named: "lowPin"))
+            return Condition(name: "Low",
+                             color: UIColor(named: "status_yellow")!,
+                             icon: UIImage(named: "lowPin"))
         case "med":
-            return Condition(name: "Runnable", color: UIColor(named: "status_green")!, icon: UIImage(named: "runnablePin"))
+            return Condition(name: "Runnable",
+                             color: UIColor(named: "status_green")!,
+                             icon: UIImage(named: "runnablePin"))
         case "high":
-            return Condition(name: "High", color: UIColor(named: "status_red")!, icon: UIImage(named: "highPin"))
+            return Condition(name: "High",
+                             color: UIColor(named: "status_red")!,
+                             icon: UIImage(named: "highPin"))
         default:
-            return Condition(name: "No Info", color: UIColor(named: "status_grey")!, icon: UIImage(named: "noinfoPin"))
+            return Condition(name: "No Info",
+                             color: UIColor(named: "status_grey")!,
+                             icon: UIImage(named: "noinfoPin"))
         }
     }
 
