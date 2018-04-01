421b802bd4b65d79a70c24ead28b54eeb69ef53e
Alex Kerney <abk@mac.com>
2018-03-31 20:44:29 -0400

    Update location info via distance filter, need to update stored values in DB

--- aw/ViewControllers/Reaches/Filters/FilterDistanceViewController.swift
+++ aw/ViewControllers/Reaches/Filters/FilterDistanceViewController.swift
@@ -6,17 +6,21 @@
 //  Copyright © 2018 Alex Kerney. All rights reserved.
 //
 
+import CoreLocation
 import UIKit
 
 class FilterDistanceViewController: UIViewController {
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
 
+    let locationManager = CLLocationManager()
+
     var distance: Float! {
         didSet {
             setDistanceLabel()
         }
     }
+    var locateTapped = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +28,7 @@ class FilterDistanceViewController: UIViewController {
         distance = DefaultsManager.distanceFilter
 
         slider.value = distance
+        locationManager.delegate = self
     }
 
     fileprivate func setDistanceLabel() {
@@ -37,10 +42,55 @@ class FilterDistanceViewController: UIViewController {
     @IBAction func distanceChanged(_ sender: UISlider) {
         distance = slider.value
     }
+
+    @IBAction func updateLocationTapped(_ sender: Any) {
+        locateTapped = true
+        setupLocationUpdates()
+    }
+}
+
+extension FilterDistanceViewController {
+    func setupLocationUpdates() {
+        let authStatus = CLLocationManager.authorizationStatus()
+
+        switch authStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .authorizedWhenInUse:
+            updateLocation()
+        default:
+            break
+        }
+    }
+
+    func updateLocation() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+    }
 }
 
 extension FilterDistanceViewController: FilterViewControllerType {
     func save() {
         DefaultsManager.distanceFilter = distance
+    }
+}
+
+extension FilterDistanceViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if locateTapped && status == .authorizedWhenInUse {
+            updateLocation()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+
+        locationManager.stopUpdatingLocation()
+        print("location updated")
+        print(location)
+
+        DefaultsManager.latitude = location.coordinate.latitude
+        DefaultsManager.longitude = location.coordinate.longitude
     }
 }
