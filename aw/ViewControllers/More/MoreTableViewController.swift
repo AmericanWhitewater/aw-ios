ab1723bc33a3bfe9b9302140af309065996aa030
Alex Kerney <abk@mac.com>
2018-04-01 10:06:01 -0400

    Removed user section from more and added AW logo jpg

--- aw/ViewControllers/More/MoreTableViewController.swift
+++ aw/ViewControllers/More/MoreTableViewController.swift
@@ -16,27 +16,24 @@ class MoreTableViewController: UITableViewController {
 
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch section {
-        case 0:
-            return 1
-        default:
-            return 5
-        }
+        return 5
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         switch indexPath {
-        case IndexPath(row: 2, section: 1): // Rate this App
+        case IndexPath(row: 2, section: 0): // Rate this App
             break
-        case IndexPath(row: 3, section: 1): // Feedback
+        case IndexPath(row: 3, section: 0): // Feedback
             break
-        case IndexPath(row: 4, section: 1): // Donate
-            UIApplication.shared.open(URL(string: "https://www.americanwhitewater.org/content/Membership/donate")!, options: [:]) { (status) in
+        case IndexPath(row: 4, section: 0): // Donate
+            UIApplication.shared.open(
+                    URL(string: "https://www.americanwhitewater.org/content/Membership/donate")!,
+                    options: [:]) { (status) in
                 if status {
                     print("Opened browser to donate page")
                 }
