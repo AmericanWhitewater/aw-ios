c0a364f5823ee6a366d6883fe6800c021eb5a698
Alex Kerney <abk@mac.com>
2018-05-04 15:23:41 -0400

    Styled fonts for about screens

--- aw/ViewControllers/More/About/AboutAWMissionViewController.swift
+++ aw/ViewControllers/More/About/AboutAWMissionViewController.swift
@@ -1,9 +1,11 @@
 import UIKit
 
 class AboutAWMissionViewController: UIViewController {
-
+    @IBOutlet weak var bodyLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        bodyLabel.apply(style: .Text1)
     }
 
     @IBAction func joinMembershipTapped(_ sender: Any) {
