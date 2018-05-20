5f2ace4c69b5fdc6272d0d4709c4e8587890520c
Alex Kerney <abk@mac.com>
2018-05-20 17:25:10 -0400

    Styling distance filter and including current location

--- aw/ViewControllers/Reaches/Filters/FilterDistanceViewController.swift
+++ aw/ViewControllers/Reaches/Filters/FilterDistanceViewController.swift
@@ -3,8 +3,13 @@ import CoreLocation
 import UIKit
 
 class FilterDistanceViewController: UIViewController {
+    @IBOutlet weak var howFarLabel: UILabel!
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var currentLocationLabel: UILabel!
+    @IBOutlet weak var cityStateLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var updateButton: UIButton!
 
     var managedObjectContext: NSManagedObjectContext?
 
@@ -20,12 +25,23 @@ class FilterDistanceViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        howFarLabel.apply(style: .Text3)
         distanceLabel.apply(style: .Text2)
+        currentLocationLabel.apply(style: .Text3)
+        cityStateLabel.apply(style: .Text3)
+        addressLabel.apply(style: .Text2)
+        addressLabel.numberOfLines = 0
+        addressLabel.lineBreakMode = .byWordWrapping
+
+        updateButton.titleLabel?.apply(style: .Text3)
+        updateButton.setTitleColor(.black, for: .normal)
 
         distance = DefaultsManager.distanceFilter
 
         slider.value = distance
         locationManager.delegate = self
+
+        displayLocation()
     }
 
     fileprivate func setDistanceLabel() {
@@ -65,6 +81,33 @@ extension FilterDistanceViewController {
             locationManager.startUpdatingLocation()
         }
     }
+
+    func displayLocation() {
+        let decoder = CLGeocoder()
+        let location = CLLocation(latitude: DefaultsManager.latitude, longitude: DefaultsManager.longitude)
+        decoder.reverseGeocodeLocation(location) { (placemarks, error) in
+            guard error == nil,
+                let placemarks = placemarks,
+                let place = placemarks.first,
+                let state = place.administrativeArea,
+                let city = place.locality
+                else {
+                    self.cityStateLabel.text = "Unknown"
+                    self.addressLabel.text = ""
+                    return }
+            self.cityStateLabel.text = "\(city), \(state)"
+
+            guard let streetNumber = place.subThoroughfare,
+                let street = place.thoroughfare,
+                let postalCode = place.postalCode
+                else {
+                    self.addressLabel.text = ""
+                    return
+            }
+
+            self.addressLabel.text = "\(streetNumber) \(street), \(city), \(state) \(postalCode)"
+        }
+    }
 }
 
 extension FilterDistanceViewController: FilterViewControllerType {
@@ -96,5 +139,6 @@ extension FilterDistanceViewController: CLLocationManagerDelegate {
         if let context = managedObjectContext {
             AWApiHelper.updateDistances(viewContext: context)
         }
+        displayLocation()
     }
 }
