8e3aae2e90ccad6b3427335ae3da758ee550341a
Alex Kerney <abk@mac.com>
2018-08-12 12:04:44 -0400

    Unwrap optional data rather than trying to force unwrap when setting image. - https://fabric.io/american-whitewater/ios/apps/org.americanwhitewater.aw/issues/5b3abc726007d59fcd75f73b?time=last-seven-days

--- aw/Extensions/UIImageView.swift
+++ aw/Extensions/UIImageView.swift
@@ -4,9 +4,10 @@ extension UIImageView {
     func loadFromUrlAsync(urlString: String) {
         if let url = URL(string: urlString) {
             DispatchQueue.global().async {
-                let data = try? Data(contentsOf: url)
-                DispatchQueue.main.async {
-                    self.image = UIImage(data: data!)
+                if let data = try? Data(contentsOf: url) {
+                    DispatchQueue.main.async {
+                        self.image = UIImage(data: data)
+                    }
                 }
             }
         }
