d692ce5af429e214c4ce7fb515b9aff5655be983
Phillip Kast <phil@yearofcode.com>
2021-09-20 15:58:18 -0700

    Remove AWApiReachHelper dependency on CoreData
    Deleting methods with no callers (that moved to ReachUpdater) to support.
    
    Now AWApiReachHelper knows nothing about persistence. It's only responsible for making calls to the old API and parsing responses. When ready, it can be replaced with GraphQL calls -- these will just need to convert responses to AWReach/AWReachDetail objects. Core Data can separately be swapped out for a different persistence mechanism without needing to change AWApiReachHelper.

--- American Whitewater/API/Reaches (non-gql)/AWApiReachHelper.swift
+++ American Whitewater/API/Reaches (non-gql)/AWApiReachHelper.swift
@@ -1,5 +1,3 @@
-import UIKit
-import CoreData
 import Alamofire
 import SwiftyJSON
 import Foundation
@@ -21,16 +19,6 @@ class AWApiReachHelper {
     typealias ReachErrorCallback = (Error) -> Void
     typealias UpdateReachesCallback = () -> Void
     typealias ReachDetailCallback = (AWReachDetail) -> Void
-    
-    private var managedObjectContext: NSManagedObjectContext {
-        (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    }
-    
-    private func privateQueueContext() -> NSManagedObjectContext {
-        let context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
-        context.parent = self.managedObjectContext
-        return context
-    }
     
     /// Calls each of the regions to be downloaded, then after all are downloaded it processes all of them at once which happens very quickly
     /// This is designed to run in the background and update the UI as it goes without delays. Users can still pull/refresh individual data while this is happening.
@@ -146,106 +134,6 @@ class AWApiReachHelper {
                     callback(reachDetails)
                 case .failure(let error):
                     callbackError(error)
-            }
-        }
-    }
-    
-    //
-    // MARK: - Duplicated in ReachUpdater -- remove when done
-    //
-    
-    private func createOrUpdateReaches(newReaches: [AWApiReachHelper.AWReach], context: NSManagedObjectContext) {
-        context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
-        
-        for newReach in newReaches {
-            let reach = findOrNewReach(newReach: newReach, context: context)
-            
-            reach.name = newReach.name
-            reach.sortName = newReach.section
-            reach.putInLat = newReach.plat
-            reach.putInLon = newReach.plon
-            reach.takeOutLat = newReach.tlat
-            reach.takeOutLon = newReach.tlon
-            reach.currentGageReading = newReach.gauge_reading // ** check
-            reach.lastGageReading = newReach.last_gauge_reading
-            reach.id = newReach.id ?? 0
-            reach.difficulty = newReach.classRating
-            reach.condition = newReach.cond
-            reach.unit = newReach.unit
-            reach.rc = newReach.rc
-            reach.delta = newReach.reading_delta
-            reach.gageId = Int32(newReach.gauge_id ?? -1)
-            reach.gageMetric = Int16(newReach.gauge_metric ?? -1)
-            reach.gageMax = newReach.gauge_max
-            reach.gageMin = newReach.gauge_min
-            reach.state = newReach.state
-            
-            reach.altname = newReach.altname
-            reach.section = newReach.section
-            
-            // API returns the total seconds before the present time as a string
-            // converting this to a Date
-            if let updatedSecondsString = newReach.last_gauge_updated,
-               let updatedSecondsAgo = Int(updatedSecondsString) {
-                reach.gageUpdated = Date().addingTimeInterval(TimeInterval(-updatedSecondsAgo))
-            }
-            
-            // calculate the distance from the user
-            if let distance = newReach.distanceFrom(location: DefaultsManager.shared.location) {
-                reach.distance = distance / 1609
-            } else {
-                reach.distance = 999999
-            }
-            
-            let difficultyRange = DifficultyHelper.parseDifficulty(difficulty: newReach.classRating ?? "")
-            if difficultyRange.contains(1) {
-                reach.difficulty1 = true
-            }
-            if difficultyRange.contains(2) {
-                reach.difficulty2 = true
-            }
-            if difficultyRange.contains(3) {
-                reach.difficulty3 = true
-            }
-            if difficultyRange.contains(4) {
-                reach.difficulty4 = true
-            }
-            if difficultyRange.contains(5) {
-                reach.difficulty5 = true
-            }
-        }
-    }
-    
-    private func findOrNewReach(newReach: AWApiReachHelper.AWReach, context: NSManagedObjectContext) -> Reach {
-        let request = Reach.reachFetchRequest() as NSFetchRequest<Reach>
-        //print("nReach name: \(newReach.name ?? "na") ID: \(NSNumber(value: newReach.id ?? 0))")
-        guard let id = newReach.id else {
-            print("invalid id: \(newReach.id ?? -1)")
-            return Reach(context: context)
-        }
-        
-        request.predicate = NSPredicate(format: "id == %i", id)
-                
-        guard let result = try? context.fetch(request), result.count > 0 else {
-            let reach = Reach(context: context)
-            reach.id = newReach.id ?? 0
-            return reach
-        }
-
-        return result.first!
-    }
-    
-    private func mergeMainContext(completion: @escaping () -> Void, errorCallback: ((Error) -> Void)? = nil) {
-        let context = managedObjectContext
-        DispatchQueue.main.async {
-            context.perform {
-                context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
-                do {
-                    try context.save()
-                    completion()
-                } catch {
-                    errorCallback?(error)
-                }
             }
         }
     }
