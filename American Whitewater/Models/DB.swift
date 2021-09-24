72fa99484e31dfdd1c6af30d4ef58c772660e40b
Phillip Kast <phil@yearofcode.com>
2021-09-23 17:08:11 -0700

    Add a singleton DB class
    It creates, migrates and vends read/write funcs for a sqlite database via GRDB

--- American Whitewater/Models/DB.swift
+++ American Whitewater/Models/DB.swift
@@ -7,3 +7,52 @@
 //
 
 import Foundation
+import GRDB
+
+class DB {
+    static let shared = try! DB()
+    
+    static let defaultDbName = "aw.sqlite"
+    
+    init(dbName: String = defaultDbName) throws {
+        url = try FileManager.default
+            .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            .appendingPathComponent(dbName)
+        
+        pool = try DatabasePool(path: url.path)
+        
+        try migrate()
+    }
+    
+    let url: URL
+    private let pool: DatabasePool
+    
+    //
+    // MARK: - read/write
+    //
+    
+    public func read<T>(_ block: (Database) throws -> T) throws -> T {
+        try pool.read(block)
+    }
+    
+    @discardableResult public func write<T>(_ block: (Database) throws -> T) throws -> T {
+        try pool.write(block)
+    }
+    
+    
+    //
+    // MARK: - Migration
+    //
+    
+    /// Migrates the database schema to the current version
+    private func migrate() throws {
+        try migrator.migrate(pool)
+    }
+    
+    private var migrator: DatabaseMigrator {
+        var m = DatabaseMigrator()
+        
+        
+        return m
+    }
+}
