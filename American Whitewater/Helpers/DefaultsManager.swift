3165bd1ec7fef6401e82377db802723fd1148f3a
Greg Lee <Gregliest@users.noreply.github.com>
2021-09-30 17:11:36 -0700

    Save last distance coordinate properly. (#266)
    * Save last distance coordinate properly.
    
    * Move to after completion of calculation.
    
    Co-authored-by: Gregory Lee <git@gregliest.com>

--- American Whitewater/Helpers/DefaultsManager.swift
+++ American Whitewater/Helpers/DefaultsManager.swift
@@ -68,6 +68,7 @@ class DefaultsManager {
                 print("Updating distances of reaches")
                 
                 reachUpdater.updateAllReachDistances{}
+                lastDistanceCoordinate = newValue
             }
         }
     }
